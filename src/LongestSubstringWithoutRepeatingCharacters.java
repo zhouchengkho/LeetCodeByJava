0b3f88898a3b982f56a57a588ef49d53f65c4ba3
cheng <c.zhou@17hi.co>
2017-04-30 13:23:47 +0800

    acceptance

--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,13 +1,31 @@
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
  * Created by zhoucheng on 4/30/17.
  */
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        int max = 0;
+        for(int i = 0; i < s.length(); i++) {
+            ArrayList temp = new ArrayList();
+            for(int j = i; j < s.length(); j++) {
+                if(temp.contains(s.charAt(j))) {
+                    max = temp.size() > max ? temp.size() : max;
+                    break;
+                } else {
+                    temp.add(s.charAt(j));
+                    max = temp.size() > max ? temp.size() : max;
+                }
+            }
+        }
+        return max;
     }
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Java means redundancy");
+        LongestSubstringWithoutRepeatingCharacters solution = new LongestSubstringWithoutRepeatingCharacters();
+        System.out.println(solution.lengthOfLongestSubstring("c"));
+
     }
 }
 
