51077ea47d96dd0d523fe09735e134c88d2a0139
cheng <c.zhou@17hi.co>
2017-04-30 12:57:03 +0800

    test

---
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,14 @@
+/**
+ * Created by zhoucheng on 4/30/17.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Java means redundancy");
+    }
+}
+
+
